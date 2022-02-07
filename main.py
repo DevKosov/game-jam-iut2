40383137ef9d362c8a83e1dc43184dda2e6a49bd
arlealexandre <arle.alexandre@protonmail.com>
2022-02-07 16:40:44 +0100

    button event

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ CURSOR = pygame.transform.scale(pygame.image.load(os.path.join('assets/img/curso
 CURSOR_RECT = CURSOR.get_rect()
 
 BACKGROUND = (173, 239, 255)
+BACKGROUND2 = (255, 0, 0)
 
 def main():
     running = True
@@ -32,16 +33,71 @@ def main():
 
     pygame.mouse.set_visible(False)
 
+    MENU = 1
+    PARTY = 0
+    OPTIONS = 0
+
     while running:
         clock.tick(FPS)
 
+        screen.fill(BACKGROUND)
+
+        ################################################################################################################
+        # VIEW
+        ################################################################################################################
+
+        if (MENU==1):
+            font = pygame.font.Font("assets/font/Pixeltype.ttf", 70)
+            # Title
+            font1 = pygame.font.Font("assets/font/Pixeltype.ttf", 70)
+            TITLE = font1.render('Nom du jeu', 0, 'Black')
+            TITLE_RECT = TITLE.get_rect(center=(WIDTH / 2, 100))
+            screen.blit(TITLE, TITLE_RECT)
+            # Button PLAY
+            font2 = pygame.font.Font("assets/font/Pixeltype.ttf", 50)
+            PLAY_BTN_LABEL = font2.render('PLAY', 0, 'Black')
+            PLAY_BTN_RECT = PLAY_BTN_LABEL.get_rect(center=(WIDTH / 2, 300))
+            PLAY_EVENT = screen.blit(PLAY_BTN_LABEL,PLAY_BTN_RECT)
+            # Button OPTIONS
+            OPT_BTN_LABEL = font2.render('OPTIONS', 0, 'Black')
+            OPT_BTN_RECT = OPT_BTN_LABEL.get_rect(center=(WIDTH / 2, 400))
+            OPT_EVENT = screen.blit(OPT_BTN_LABEL,OPT_BTN_RECT)
+            # Button CREDITS
+            CRED_BTN_LABEL = font2.render('CREDITS', 0, 'Black')
+            CRED_BTN_RECT = CRED_BTN_LABEL.get_rect(center=(WIDTH / 2, 500))
+            screen.blit(CRED_BTN_LABEL,CRED_BTN_RECT)
+            # Button EXIT
+            EXIT_BTN_LABEL = font2.render('EXIT', 0, 'Black')
+            EXIT_BTN_RECT = EXIT_BTN_LABEL.get_rect(center=(WIDTH / 2, 600))
+            EXIT_EVENT = screen.blit(EXIT_BTN_LABEL,EXIT_BTN_RECT)
+
+        elif (PARTY==1):
+            screen.fill(BACKGROUND2)
+        
+        elif (OPTIONS==1):
+            # Title
+            font1 = pygame.font.Font("assets/font/Pixeltype.ttf", 70)
+            TITLE = font1.render('OPTIONS', 0, 'Black')
+            TITLE_RECT = TITLE.get_rect(center=(WIDTH / 2, 100))
+            screen.blit(TITLE, TITLE_RECT)
+
+        ################################################################################################################
+        # EVENT LISTENER
+        ################################################################################################################
+        
         for event in pygame.event.get():
-
-            if event.type == pygame.KEYDOWN:
+            if event.type == pygame.QUIT or event.type == pygame.MOUSEBUTTONUP and event.button==1 and EXIT_EVENT.collidepoint(event.pos):
+                running = False
+                exit()
+            elif event.type == pygame.KEYDOWN:
                 player.update(event)
+            elif event.type == pygame.MOUSEBUTTONUP and event.button==1 and PLAY_EVENT.collidepoint(event.pos):
+                MENU=0
+                PARTY=1
+            elif event.type == pygame.MOUSEBUTTONUP and event.button==1 and OPT_EVENT.collidepoint(event.pos):
+                MENU=0
+                OPTIONS=1
 
-        # Title screen
-        titleScreen()
 
         player.draw(screen)
 
@@ -50,53 +106,7 @@ def main():
         CURSOR_RECT.center = mouse_pos
         screen.blit(CURSOR, CURSOR_RECT)
 
-        
-                
-
         pygame.display.update()
-
-def titleScreen():
-
-    # background color of the window
-    screen.fill(BACKGROUND)
-
-    # font
-    font = pygame.font.Font("assets/font/Pixeltype.ttf", 70)
-
-    # Title
-    font1 = pygame.font.Font("assets/font/Pixeltype.ttf", 70)
-    TITLE = font1.render('Nom du jeu', 0, 'Black')
-    TITLE_RECT = TITLE.get_rect(center=(WIDTH / 2, 100))
-    screen.blit(TITLE, TITLE_RECT)
-
-    # Button PLAY
-    font2 = pygame.font.Font("assets/font/Pixeltype.ttf", 50)
-    PLAY_BTN_LABEL = font1.render('PLAY', 0, 'Black')
-    PLAY_BTN_RECT = PLAY_BTN_LABEL.get_rect(center=(WIDTH / 2, 300))
-    PLAY_EVENT = screen.blit(PLAY_BTN_LABEL,PLAY_BTN_RECT)
-
-    # Button OPTIONS
-    OPT_BTN_LABEL = font1.render('OPTIONS', 0, 'Black')
-    OPT_BTN_RECT = OPT_BTN_LABEL.get_rect(center=(WIDTH / 2, 400))
-    screen.blit(OPT_BTN_LABEL,OPT_BTN_RECT)
-
-    # Button CREDITS
-    CRED_BTN_LABEL = font1.render('CREDITS', 0, 'Black')
-    CRED_BTN_RECT = CRED_BTN_LABEL.get_rect(center=(WIDTH / 2, 500))
-    screen.blit(CRED_BTN_LABEL,CRED_BTN_RECT)
-
-    # Button EXIT
-    EXIT_BTN_LABEL = font1.render('EXIT', 0, 'Black')
-    EXIT_BTN_RECT = EXIT_BTN_LABEL.get_rect(center=(WIDTH / 2, 600))
-    EXIT_EVENT = screen.blit(EXIT_BTN_LABEL,EXIT_BTN_RECT)
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT or event.type == pygame.MOUSEBUTTONUP and event.button==1 and EXIT_EVENT.collidepoint(event.pos):
-            running = False
-            exit()
-        elif event.type == pygame.MOUSEBUTTONUP and event.button==1 and PLAY_EVENT.collidepoint(event.pos):
-            print("PLAY CLICKED")
-
 
 if __name__ == "__main__":
     main()
