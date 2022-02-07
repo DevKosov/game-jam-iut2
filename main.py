3d520438cc0cb2095627c938f8597fe6de106293
arlealexandre <arle.alexandre@protonmail.com>
2022-02-07 17:21:59 +0100

    Merge branch 'main' of https://github.com/DevKosov/game-jam-iut2 into main

--- main.py
+++ main.py
@@ -20,6 +20,9 @@ player.add(Player.Player())
 BACKGROUND = (173, 239, 255)
 BACKGROUND2 = (255, 0, 0)
 
+def getFont(size):
+    return pygame.font.Font("assets/font/Pixeltype.ttf", size)
+
 def main():
     running = True
     clock = pygame.time.Clock()
@@ -29,6 +32,10 @@ def main():
     pygame.mixer.music.play(fade_ms=2000)
 
     pygame.mouse.set_visible(False)
+
+    font1 = pygame.font.Font("assets/font/Pixeltype.ttf", 70)
+    font2 = pygame.font.Font("assets/font/Pixeltype.ttf", 50)
+    font3 = pygame.font.Font("assets/font/Pixeltype.ttf", 30)
 
     MENU = 1
     PARTY = 0
@@ -46,49 +53,43 @@ def main():
 
         if (MENU==1):
             # Title
-            font1 = pygame.font.Font("assets/font/Pixeltype.ttf", 70)
-            TITLE = font1.render('Nom du jeu', 0, 'Black')
+            TITLE = getFont(70).render('Nom du jeu', 0, 'Black')
             TITLE_RECT = TITLE.get_rect(center=(WIDTH / 2, 100))
             screen.blit(TITLE, TITLE_RECT)
             # Button PLAY
-            font2 = pygame.font.Font("assets/font/Pixeltype.ttf", 50)
-            PLAY_BTN_LABEL = font2.render('PLAY', 0, 'Black')
+            PLAY_BTN_LABEL = getFont(50).render('PLAY', 0, 'Black')
             PLAY_BTN_RECT = PLAY_BTN_LABEL.get_rect(center=(WIDTH / 2, 300))
             PLAY_EVENT = screen.blit(PLAY_BTN_LABEL,PLAY_BTN_RECT)
             # Button OPTIONS
-            OPT_BTN_LABEL = font2.render('OPTIONS', 0, 'Black')
+            OPT_BTN_LABEL = getFont(50).render('OPTIONS', 0, 'Black')
             OPT_BTN_RECT = OPT_BTN_LABEL.get_rect(center=(WIDTH / 2, 400))
             OPT_EVENT = screen.blit(OPT_BTN_LABEL,OPT_BTN_RECT)
             # Button CREDITS
-            CRED_BTN_LABEL = font2.render('CREDITS', 0, 'Black')
+            CRED_BTN_LABEL = getFont(50).render('CREDITS', 0, 'Black')
             CRED_BTN_RECT = CRED_BTN_LABEL.get_rect(center=(WIDTH / 2, 500))
             CRED_EVENT = screen.blit(CRED_BTN_LABEL,CRED_BTN_RECT)
             # Button EXIT
-            EXIT_BTN_LABEL = font2.render('EXIT', 0, 'Black')
+            EXIT_BTN_LABEL = getFont(50).render('EXIT', 0, 'Black')
             EXIT_BTN_RECT = EXIT_BTN_LABEL.get_rect(center=(WIDTH / 2, 600))
             EXIT_EVENT = screen.blit(EXIT_BTN_LABEL,EXIT_BTN_RECT)
 
         elif (OPTIONS==1):
             # Title
-            font1 = pygame.font.Font("assets/font/Pixeltype.ttf", 70)
-            TITLE = font1.render('OPTIONS', 0, 'Black')
+            TITLE = getFont(70).render('OPTIONS', 0, 'Black')
             TITLE_RECT = TITLE.get_rect(center=(WIDTH / 2, 100))
             screen.blit(TITLE, TITLE_RECT)
             # Back to menu
-            font2 = pygame.font.Font("assets/font/Pixeltype.ttf", 30)
-            BACK = font2.render('BACK TO MENU', 0, 'Black')
+            BACK = getFont(30).render('BACK TO MENU', 0, 'Black')
             BACK_RECT = BACK.get_rect(center=(WIDTH / 12, 30))
             BACK_EVENT = screen.blit(BACK, BACK_RECT)
 
         elif (CREDITS==1):
             # Title
-            font1 = pygame.font.Font("assets/font/Pixeltype.ttf", 70)
-            TITLE = font1.render('CREDITS', 0, 'Black')
+            TITLE = getFont(70).render('CREDITS', 0, 'Black')
             TITLE_RECT = TITLE.get_rect(center=(WIDTH / 2, 100))
             screen.blit(TITLE, TITLE_RECT)
             # Back to menu
-            font2 = pygame.font.Font("assets/font/Pixeltype.ttf", 30)
-            BACK = font2.render('BACK TO MENU', 0, 'Black')
+            BACK = getFont(30).render('BACK TO MENU', 0, 'Black')
             BACK_RECT = BACK.get_rect(center=(WIDTH / 12, 30))
             BACK_EVENT = screen.blit(BACK, BACK_RECT)
 
