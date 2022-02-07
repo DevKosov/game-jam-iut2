21e00b4fc5f35f83abbb684802dae99c1a65b7de
RomainMIRAS <73794188+RomainMIRAS@users.noreply.github.com>
2022-02-07 15:41:08 +0100

    spritesheet insane drew

--- Player.py
+++ Player.py
@@ -6,17 +6,23 @@ class Player(pygame.sprite.Sprite):
 		super().__init__()
 
 		try:
-			self.sheet = pygame.image.load(os.path.join('assets/img/characters/', 'doux.png')).convert()
+			self.sheet_image = pygame.image.load(os.path.join('assets/img/characters/', 'doux.png')).convert()
 		except pygame.error as e:
 			print(f"Unable to load spritesheet image: player spritesheet ")
 			raise SystemExit(e)
 
+		self.sheet = spritesheet.SpriteSheet(self.sheet_image)
+
 		# self.player_walk = [player_image,player_image]
 
-		self.player_index = 0
+		BLACK = (0, 0, 0)
 
-		self.image = self.sheet #self.player_walk[self.player_index]
-		self.rect = self.image.get_rect(midbottom = (80,300))
+		self.image = self.sheet.get_image(0, 24, 24, 3, BLACK)
+		# frame_1 = sprite_sheet.get_image(1, 24, 24, 3, BLACK)
+		# frame_2 = sprite_sheet.get_image(2, 24, 24, 3, BLACK)
+		# frame_3 = sprite_sheet.get_image(3, 24, 24, 3, BLACK)
+
+		self.rect = self.sheet_image.get_rect(midbottom = (80,300))
 		self.gravity = 0
 
 		self.jump_sound = pygame.mixer.Sound('assets/audio/se/Bell3.ogg')
