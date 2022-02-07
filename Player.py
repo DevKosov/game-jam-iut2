d167b99d1dc7ca2dc447ca9746a2d7f85191f2ff
RDel-Medico <95379424+RDel-Medico@users.noreply.github.com>
2022-02-07 18:00:12 +0100

    Update Player.py

--- Player.py
+++ Player.py
@@ -26,6 +26,8 @@ class Player(pygame.sprite.Sprite):
 		self.frame_6 = self.sheet.get_image(7,1, 24, 24, 3,BLACK)
 		self.frame_7 = self.sheet.get_image(8,1, 24, 24, 3,BLACK)
 
+		self.rotation = 1 # 1 regarde à droite // 2 regarde à gauche
+
 		self.index = 0;
 
 		self.image = self.frame_0
@@ -51,26 +53,35 @@ class Player(pygame.sprite.Sprite):
 		#self.index = int(self.index)
 		if self.index == 0:
 			self.image = self.frame_0
+			self.flipper()
 		if self.index >= 1:
 			self.image = self.frame_1
+			self.flipper()
 		if self.index >= 2:
 			self.image = self.frame_2
+			self.flipper()
 		if self.index >= 3:
 			self.image = self.frame_3
+			self.flipper()
 		if self.index >= 4:
 			self.image = self.frame_4
+			self.flipper()
 		if self.index >= 5:
 			self.image = self.frame_5
+			self.flipper()
 		if self.index >= 6:
 			self.image = self.frame_6
+			self.flipper()
 		if self.index >= 7:
 			self.image = self.frame_7
+			self.flipper()
 
 
 	def update(self, event = None):
 		#self.player_input(event)
 		self.animation_state()
 		self.move()
+		self.image.set_colorkey((0,0,0))
 
 	def move(self):
 		keys_pressed = pygame.key.get_pressed()
@@ -79,22 +90,29 @@ class Player(pygame.sprite.Sprite):
 			self.index += 0.2
 			if self.index >= 7:
 				self.index = 0
+
+			if self.rotation == 1:
+				self.rotation = 2
 		if keys_pressed[pygame.K_RIGHT]:
 			self.rect.left = self.rect.left + 5
 			self.index += 0.2
 			if self.index >= 7:
 				self.index = 0
+			if self.rotation == 2:
+				self.rotation = 1
 		if keys_pressed[pygame.K_UP]:
 			self.rect.top = self.rect.top - 5
 			self.index += 0.2
 			if self.index >= 7:
 				self.index = 0
+			self.rotation(self.rotation, 0)
 		if keys_pressed[pygame.K_DOWN]:
 			self.rect.top = self.rect.top + 5
 			self.index += 0.2
 			if self.index >= 7:
 				self.index = 0
-		print(self.index)
 
 		
-		
+	def flipper(self):
+		if self.rotation != 1:
+				self.image = pygame.transform.flip(self.image, 1, 0)
