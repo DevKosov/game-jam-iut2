d42de68c4e0c545bfb92ec3e0a8b698bab1aee18
RDel-Medico <95379424+RDel-Medico@users.noreply.github.com>
2022-02-07 15:52:25 +0100

    space

--- main.py
+++ main.py
@@ -34,7 +34,10 @@ def main():
 
     while running:
         clock.tick(FPS)
+        for event in pygame.event.get():
 
+            if event.type == pygame.KEYDOWN:
+                player.update(event)
         # Title screen
         titleScreen()
 
@@ -45,10 +48,7 @@ def main():
         CURSOR_RECT.center = mouse_pos
         screen.blit(CURSOR, CURSOR_RECT)
 
-        for event in pygame.event.get():
-
-            if event.type == pygame.KEYDOWN:
-                player.update()
+        
                 
 
         pygame.display.update()
