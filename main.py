64b14d0d9711ae74848506fd318b28d3e878c259
Kc-sound <Kc-sound@gmail.com>
2022-02-07 12:47:52 +0100

    main bases

---
+++ main.py
@@ -0,0 +1,29 @@
+import pygame
+from sys import exit
+
+pygame.init()
+
+WIDTH, HEIGHT = 1024, 768
+FPS = 60
+
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+
+
+def main():
+    running = True
+    clock = pygame.time.Clock()
+
+    while running:
+        clock.tick(FPS)
+        WIN.fill('Yellow')
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                exit()
+
+
+        pygame.display.update()
+
+
+if __name__ == "__main__":
+    main()
